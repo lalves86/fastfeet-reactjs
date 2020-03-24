31e8a3a674b710db1be787d286a1dbaec7bd6773
lalves86 <lucas.alves@engenharia.ufjf.br>
2020-03-24 10:34:45 -0300

    Estrutura inicial

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function signIn() {
+  return <h1>Sign In</h1>;
+}
---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
+import SignIn from '~/pages/SignIn';
+import Orders from '~/pages/Orders';
+import Recipients from '~/pages/Recipients';
+import Deliverers from '~/pages/Deliverers';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/orders" isPrivate component={Orders} />
+      <Route path="/recipients" isPrivate component={Recipients} />
+      <Route path="/deliverers" isPrivate component={Deliverers} />
+      <Route path="/" component={() => <h1>404 - Not found!</h1>} />
+    </Switch>
+  );
+}
