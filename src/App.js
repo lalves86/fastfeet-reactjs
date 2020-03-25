06c5da4599d1aa40cb5abe77de4f491b79b408c5
lalves86 <lucas.alves@engenharia.ufjf.br>
2020-03-25 09:38:33 -0300

    Configurando store do redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
 
+import store from './store';
 import Routes from './routes';
 import history from './services/history';
 import GlobaStyle from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <GlobaStyle />
-      <Routes />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <GlobaStyle />
+        <Routes />
+      </Router>
+    </Provider>
   );
 }
 
